08d42294ad31bd9b2a09cd7cd1a34c8578c6b468
Iris Y <48094822+irisyann@users.noreply.github.com>
2023-01-29 18:35:00 +0800

    Update pexels.js

--- src/pexels.js
+++ src/pexels.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: `https://api.pexels.com/`,
     headers: {
-        Authorization: '563492ad6f91700001000001c7912b3d1ca84ce3921b9f846a8bd684'
+        Authorization: process.ENV.PEXELS_API_KEY
     }
 });
