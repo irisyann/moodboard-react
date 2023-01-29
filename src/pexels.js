ffd94a3b9901d5e3827e9dd95473ff02a8024e90
Iris Y <48094822+irisyann@users.noreply.github.com>
2023-01-29 18:40:29 +0800

    Update pexels.js

--- src/pexels.js
+++ src/pexels.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: `https://api.pexels.com/`,
     headers: {
-        Authorization: process.ENV.PEXELS_API_KEY
+        Authorization: process.ENV.REACT_APP_PEXELS_API_KEY
     }
 });
